57f7ffcb78d482a318c4326cf124a864117e6790
Ivanyany <754339948@qq.com>
2019-08-23 15:38:02 +0800

    springboot整合dubbo成功并可以测试

--- imall-user-service/src/main/java/com/ivan/imall/user/ImallUserServiceApplication.java
+++ imall-user-service/src/main/java/com/ivan/imall/user/ImallUserServiceApplication.java
@@ -1,10 +1,12 @@
 package com.ivan.imall.user;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@EnableDubbo
 @MapperScan(basePackages = "com.ivan.imall.user.mapper")
 public class ImallUserServiceApplication {
 
--- imall-user-web/src/main/java/com/ivan/imall/user/controller/UserController.java
+++ imall-user-web/src/main/java/com/ivan/imall/user/controller/UserController.java
@@ -4,7 +4,6 @@ import com.ivan.imall.bean.UmsMember;
 import com.ivan.imall.bean.UmsMemberReceiveAddress;
 import com.ivan.imall.service.UserService;
 import com.alibaba.dubbo.config.annotation.Reference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,7 +21,8 @@ public class UserController {
     @Reference
     UserService userService;
 
-    @RequestMapping("getReceiveAddressByMemberId")
+    @RequestMapping("/getReceiveAddressByMemberId")
+    @ResponseBody
     public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
 
         List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
@@ -31,20 +31,19 @@ public class UserController {
     }
 
 
-    @RequestMapping("getAllUser")
+    @RequestMapping("/getAllUser")
     @ResponseBody
     public List<UmsMember> getAllUser(){
 
         List<UmsMember> umsMembers = userService.getAllUser();
+
         return umsMembers;
     }
 
-    @RequestMapping("index")
+    @RequestMapping("/index")
     @ResponseBody
     public String index(){
         return "hello user";
     }
-
-
 
 }
