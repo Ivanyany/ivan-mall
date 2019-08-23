40b14c38afa31c71d5ca880d83fb11cd040cdeb6
Ivanyany <754339948@qq.com>
2019-08-23 14:39:33 +0800

    上传imall-user项目到GitHub

---
+++ imall-user-web/src/main/java/com/ivan/imall/user/ImallUserWebApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.imall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImallUserWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImallUserWebApplication.class, args);
+	}
+
+}
---
+++ imall-user-web/src/main/java/com/ivan/imall/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.ivan.imall.user.controller;
+
+import com.ivan.imall.bean.UmsMember;
+import com.ivan.imall.bean.UmsMemberReceiveAddress;
+import com.ivan.imall.service.UserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @Auther: Ivan
+ * @Date: 2019/8/23 11:56
+ * @Description:
+ */
+@Controller
+public class UserController {
+
+    @Reference
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+
+        return umsMemberReceiveAddresses;
+    }
+
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+
+        List<UmsMember> umsMembers = userService.getAllUser();
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+
+
+
+}
